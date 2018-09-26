b432faf94e0940fb5c2dae37b3db33ad85ab41de
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2018-09-27 01:14:05 +0300

    Change secret signer address

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -9,5 +9,5 @@ export default {
         uri: 'http://localhost:8545',
     },
     // The address corresponding to a private key used to sign placeBet commits.
-    secretSigner: '0x3e4C638383D0DDb11e4B40D6522Fdf1A7A49Bb9c',
+    secretSigner: '0x3397CdeF1501B1DA81e00eB9685E75B5e7DcE231',
 };
